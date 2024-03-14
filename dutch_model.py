f196bc071786351a878b9ccd5168e55a68e987c9
Wessel Heerema <w.heerema@student.rug.nl>
2024-03-14 10:31:42 +0100

    Put all functions into class BERTonSVMClassifier

--- dutch_model.py
+++ dutch_model.py
@@ -4,49 +4,48 @@ from transformers import AutoTokenizer, AutoModel, TFAutoModel
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score
 
-# Load BERTje tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("GroNLP/bert-base-dutch-cased")
-model = AutoModel.from_pretrained("GroNLP/bert-base-dutch-cased")  # PyTorch
 
-# Load data
-data_frame = pd.read_csv("TRAIN/train_data_offensive_abusive_taskC.csv")
+class BERTonSVMClassifier:
+    def __init__(self, model_url="GroNLP/bert-base-dutch-cased"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_url)
+        self.model = AutoModel.from_pretrained(model_url)  # PyTorch
+    
+    def data_split(self, df_url, train_len=0.008, test_len=0.002):
+        """Split the data according to the specified proportions"""
+        data_frame = pd.read_csv(df_url)
+        df_len = len(data_frame)
 
-# split data 80 / 20
-length_data_frame = len(data_frame)
+        # Shuffle the DataFrame to ensure randomness
+        df_shuffled = data_frame.sample(frac=1, random_state=42)
 
-# Shuffle the DataFrame to ensure randomness
-data_frame_shuffled = data_frame.sample(frac=1, random_state=42)
+        df_train = df_shuffled.tail(int(df_len*train_len))
+        train_data = df_train['text'].tolist()
+        train_labels = df_train['abusive_offensive_not'].tolist()
 
+        df_test = df_shuffled.head(int(df_len*test_len))
+        test_data = df_test['text'].tolist()
+        test_labels = df_test['abusive_offensive_not'].tolist()
 
-train_data_frame = data_frame_shuffled.tail(int(length_data_frame*0.08))
-train_data = train_data_frame['text'].tolist()
-train_labels = train_data_frame['abusive_offensive_not'].tolist()
+        return train_data, train_labels, test_data, test_labels
+    
+    def tokenize(self, indata):
+        """Tokenize and obtain BERTje embeddings for training data"""
+        input_ids = self.tokenizer(indata, padding=True,
+                                         truncation=True, return_tensors="pt")
+        with torch.no_grad():
+            train_outputs = self.model(**input_ids)
+        return train_outputs.last_hidden_state.mean(dim=1).numpy()
 
-test_data_frame = data_frame_shuffled.head(int(length_data_frame*0.02))
-test_data = test_data_frame['text'].tolist()
-test_labels = test_data_frame['abusive_offensive_not'].tolist()
+    def train(self, embeds, labels):
+        """Train SVM classifier"""
+        self.svm_classifier = SVC(kernel='linear')
+        self.svm_classifier.fit(embeds, labels)
 
+    def predict(self, embeds):
+        """Predict test data"""
+        return self.svm_classifier.predict(embeds)
 
-# Tokenize and obtain BERTje embeddings for training data
-train_input_ids = tokenizer(train_data, padding=True, truncation=True, return_tensors="pt")
-with torch.no_grad():
-    train_outputs = model(**train_input_ids)
-train_embeddings = train_outputs.last_hidden_state.mean(dim=1).numpy()
-
-
-# Train SVM classifier
-svm_classifier = SVC(kernel='linear')
-svm_classifier.fit(train_embeddings, train_labels)
-
-# Tokenize and obtain BERTje embeddings for test data
-test_input_ids = tokenizer(test_data, padding=True, truncation=True, return_tensors="pt")
-with torch.no_grad():
-    test_outputs = model(**test_input_ids)
-test_embeddings = test_outputs.last_hidden_state.mean(dim=1).numpy()
-
-# Predict on test data
-predictions = svm_classifier.predict(test_embeddings)
-
-# Calculate F1 score
-f1 = f1_score(test_labels, predictions, average='macro')
-print("F1 Score:", f1)
+    def metrics(self, true, predicted):
+        """Calculate F1 score"""
+        f1 = f1_score(true, predicted, average='macro')
+        print("F1 Score:", f1)
