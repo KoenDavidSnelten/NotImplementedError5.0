17c2a760f77966d20e5830886f691fc045db6cae
Wessel Heerema <w.heerema@student.rug.nl>
2024-03-21 16:06:04 +0100

    more code from koen; needs troubleshooting

--- evaluate.py
+++ evaluate.py
@@ -4,16 +4,19 @@ import sys
 
 def eval(infile, bert, svm):
     # load models
-    tokenizer = AutoTokenizer.from_pretrained(bert)
+    tokenizer = AutoTokenizer.from_pretrained('vinai/bertweet-base')
     model = AutoModel.from_pretrained(bert)  # PyTorch
     # load dev_data_text
     df_test = pd.read_csv(infile)
     # tokenize
     tokenized = tokenizer(df_test["text"], padding="max_length",
                           truncation=True)
+    
+    label_map_BERTweet = {0: "ABUSIVE", 1: "NOT", 2: "OFFENSIVE"}
+
     # predict labels
-    model.eval()
     predicted = model.predict(tokenized)
+
     # make output file
     df_test["abusive_offensive_not"] = predicted
     df_test.to_csv('test_set_out.csv', index=False)
