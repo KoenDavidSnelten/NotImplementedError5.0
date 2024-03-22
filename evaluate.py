8db2edbc7fb3154d16b89b98e307f33dd2f06a04
Twan Huiskens <huiskenstwan@gmail.com>
2024-03-22 16:28:19 +0100

    Add argument parser and fix loading of the model

--- evaluate.py
+++ evaluate.py
@@ -1,16 +1,19 @@
+import os
+from argparse import ArgumentParser
 import pandas as pd
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
 import sys
 
-def evaluate(infile, bert, svm):
+def evaluate(infile, bert):
     # load models
     tokenizer = AutoTokenizer.from_pretrained('vinai/bertweet-base')
-    model = AutoModel.from_pretrained(bert)  # PyTorch
+    model = AutoModelForSequenceClassification.from_pretrained(bert, use_safetensors=True)
     # load dev_data_text
     df_test = pd.read_csv(infile)
     # tokenize
     tokenized = tokenizer(df_test["text"], padding="max_length",
                           truncation=True)
+
     label_map_BERTweet = {0: "ABUSIVE", 1: "NOT", 2: "OFFENSIVE"}
 
     # predict labels
@@ -22,4 +25,23 @@ def evaluate(infile, bert, svm):
 
 
 if __name__ == "__main__":
-    evaluate(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    parser = ArgumentParser(
+        description="Evaluate the model on the given test data."
+    )
+
+    # Add bertweet
+    import sys
+    sys.path.append('BERTweet')
+
+    parser.add_argument("infile", help="Path to the (dev) data input file")
+    parser.add_argument("model", help="Path to the model")
+    parser.add_argument("output", help="Path to the output file")
+
+    args = parser.parse_args()
+
+    model_path = os.path.join(os.getcwd(), args.model)
+    print(model_path)
+
+    evaluate(args.infile, model_path)
+
